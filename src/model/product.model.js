3cf2c5dcf668d32bc26654cf2528d02603079cc2
sofiabrussa <sofiabrussa@gmail.com>
2023-10-03 22:41:11 -0300

    rout

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -2,12 +2,13 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
 
-const sequelize = new Sequelize( "product_test", "root", "12345", {
+const sequelize = new Sequelize( "product_test", "root", "12345", {  // Cuando se quiera acceder a cualquier modelo o hacer alguna consulta se debe acceder a través de la varaible sequelize.
     host: "localhost",  //Aca va la direccion de la base de datos pero ahora lo voy a hacer local asiq va local, si fuera una bse de datos externa, se pondria la dire
     dialect: "mysql", 
     port: 3306  
     
 })
+
 
 async function testConnection() {
     try {
@@ -23,10 +24,11 @@ async function testConnection() {
 testConnection();
 
 
-class Product extends Model {}
+class Product extends Model {}  //// Create a model 
     
-Product.init({
+Product.init({   //Inicializar el model
     product_id:  {
+        //Definimos atributos del model
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -1,7 +1,8 @@
-//os routers son especialmente útiles en aplicaciones web para gestionar las rutas o URLs a las que los usuarios pueden acceder. 
+//Los routers son especialmente útiles en aplicaciones web para gestionar las rutas o URLs a las que los usuarios pueden acceder. 
 //Puedes definir rutas específicas y asociarlas con funciones o controladores que se ejecutarán cuando se acceda a esas rutas.
 
 const router = require("express").Router()
+const Products = require("../model/product.model")
 
 router.get("/products", (req, res) =>  {  //Un get para llamar a todos mis productos
     res.send("i am a Router")
