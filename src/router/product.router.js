b36ed380c4b65faea9e7188090d286319670b9e5
sofiabrussa <sofiabrussa@gmail.com>
2023-10-11 20:06:20 -0300

    metodo get y post

--- src/router/product.router.js
+++ src/router/product.router.js
@@ -3,18 +3,45 @@
 
 const router = require("express").Router()
 const Products = require("../model/product.model")
+const { faker } = require("@faker-js/faker") //genera de datos ficticios
 
-router.get("/products", (req, res) =>  {  //Un get para llamar a todos mis productos
-    res.send("i am a Router")
+router.get("/products", async (req, res) =>  {  //Un get para llamar a todos mis productos
+    const products = await Products.findAll() //findall se usa para llamar a todos los elementos de mi tabla
+    res.status(200).json( {
+        ok: true,
+        status: 200,
+        body: products   //cuerpo de la peticion, los productos
+    })
 });
 
-router.get("/products", (req, res) =>  {  //Otro get para traer un solo producto
-    res.send("i am a Router")
+router.get("/products/:product_id", async (req, res) =>  {  //Otro get para traer un solo producto. Es importante poner los : 
+    const id = req.params.product_id;   
+    const product = await Products.findOne({  //findone porque buscamos un solo producto
+        where:  {
+            product_id: id,
+        }
+    }) 
+    res.status(200).json( {
+        ok: true,
+        status: 200,
+        body: product   
+    })
 })
 
-router.post("/products", (req, res) =>  { //Para crear nuevos productos
-    res.send("i am a Router")
+router.post("/products", async (req, res) =>  {    //Para crear nuevos productos
+   await Products.sync()
+   const createProduct = await Products.create({
+    product_name: faker.commerce.product(),
+    price: faker.commerce.price(),
+    is_stock: faker.datatype.boolean()
+   })
+   res.status(201).json({
+    ok: true,
+    status: 201,
+    message: "Created Product"
+    })
 });
+
 
 router.put("/products", (req, res) =>  {  //Para actualizar productos
     res.send("i am a Router")
